bb0d0c60d18bbb478acfdab36d5d626de465bf60
Igor <Igor-87@mail.ru>
2023-02-20 14:51:32 +0300

    added css styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,23 @@
 import './lib/lib';
 
-
-$('button').on('click', function(){
-    $('div').eq(2).toggleClass('active');
-});
-
-$('div').click(function () {
+// $('button').on('click', function(){
+//     $('div').eq(2).toggleClass('active');
+// });
+// $('div').click(function () {
 // console.log($(this).index());
-});
-
+// });
 // console.log($('div').eq(2).find('.more'));
-
 // console.log($('button').html('Hello'));
-
 // console.log($('.some').closest('.findmeG'));
-
 // console.log($('.more').eq(0).siblings());
+// $('button').fadeIn(1800);
 
-$('button').fadeIn(1800);
+$('#first').on('click',() =>{
+    $('div').eq(1).fadeOut(800);
+});
+$('[data-count = "second"]').on('click',() =>{
+    $('div').eq(2).fadeOut(800);
+});
+$('button').eq(2).on('click',() =>{
+    $('.w-500').fadeOut(800);
+});
