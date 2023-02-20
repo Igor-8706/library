154fbeb928e2dccde285d56c3e046916b2e15563
Igor <Igor-87@mail.ru>
2023-02-20 19:24:00 +0300

    added modal window

--- src/js/main.js
+++ src/js/main.js
@@ -22,15 +22,16 @@ $('button').eq(2).on('click', () => {
     $('.w-500').fadeToggle(800);
 });
 
-$('.wrap').html(
-    ` <div class="dropdown">
-    <button class="btn btn-primary dropdown-toggle" id="drowdownMenuButton">Dropdown button</button>
-    <div class="dropdown-menu" data-toggle-id="drowdownMenuButton">
-        <a href="#" class="dropdown-item">Action</a>
-        <a href="#" class="dropdown-item">Action #2</a>
-        <a href="#" class="dropdown-item">Action #3</a>
-    </div>
-</div>
-</div>`
-);
-$('.dropdown-toggle').dropdown();
+// $('.wrap').html(
+//     ` <div class="dropdown">
+//     <button class="btn btn-primary dropdown-toggle" id="drowdownMenuButton">Dropdown button</button>
+//     <div class="dropdown-menu" data-toggle-id="drowdownMenuButton">
+//         <a href="#" class="dropdown-item">Action</a>
+//         <a href="#" class="dropdown-item">Action #2</a>
+//         <a href="#" class="dropdown-item">Action #3</a>
+//     </div>
+// </div>
+// </div>`
+// );
+// $('.dropdown-toggle').dropdown();
+
