5360e284eceab2dcf3a434c8480b7e3f55724b24
Igor <Igor-87@mail.ru>
2023-02-20 18:04:14 +0300

    added dropdown menu

--- src/js/main.js
+++ src/js/main.js
@@ -12,12 +12,25 @@ import './lib/lib';
 // console.log($('.more').eq(0).siblings());
 // $('button').fadeIn(1800);
 
-$('#first').on('click',() =>{
+$('#first').on('click', () => {
     $('div').eq(1).fadeToggle(800);
 });
-$('[data-count = "second"]').on('click',() =>{
+$('[data-count = "second"]').on('click', () => {
     $('div').eq(2).fadeToggle(800);
 });
-$('button').eq(2).on('click',() =>{
+$('button').eq(2).on('click', () => {
     $('.w-500').fadeToggle(800);
 });
+
+$('.wrap').html(
+    ` <div class="dropdown">
+    <button class="btn btn-primary dropdown-toggle" id="drowdownMenuButton">Dropdown button</button>
+    <div class="dropdown-menu" data-toggle-id="drowdownMenuButton">
+        <a href="#" class="dropdown-item">Action</a>
+        <a href="#" class="dropdown-item">Action #2</a>
+        <a href="#" class="dropdown-item">Action #3</a>
+    </div>
+</div>
+</div>`
+);
+$('.dropdown-toggle').dropdown();
